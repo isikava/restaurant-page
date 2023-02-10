fb03a6fdff171bc6f12224db900fe74ea78cd947
Alexander Ignatev <alexign473@gmail.com>
2023-02-10 18:33:14 +0300

    Add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,14 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
